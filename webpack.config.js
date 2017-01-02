f09733f98ebaa60adf1bc654f457a2d2af14b9b0
rachelmad <rachelvmadrigal@gmail.com>
2017-01-01 23:29:09 -0600

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: "./src/App.js",
+  output: {
+  	path: "./static",
+    filename: "bundle.js"
+  },
+  watch: true,
+  module: {
+   loaders: [
+     {
+       test: [/\.js$/, /\.es6$/],
+       exclude: /node_modules/,
+       loader: 'babel-loader',
+       query: {
+         presets: ['react', 'es2015'] 
+       }
+     }
+   ]
+ },
+ resolve: {
+   extensions: ['', '.js', '.es6']
+ }
+}
