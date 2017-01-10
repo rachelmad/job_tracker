6ca094b67557127c19a94d1c2afc2cd24addf7e6
rachelmad <rachelvmadrigal@gmail.com>
2017-01-09 22:46:40 -0600

    Added basic Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Route, Router, hashHistory } from "react-router";
+
 import JobsOverview from "./components/JobsOverview";
 
-ReactDOM.render(
-  <JobsOverview />,
+ReactDOM.render((
+    <Router history={hashHistory}>
+      <Route path="/" component={JobsOverview} />
+    </Router>
+  ),
   document.getElementById('main')
 );
