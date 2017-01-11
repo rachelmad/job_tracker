f972cb1204a61b7de2cd9652066c121100cb250b
rachelmad <rachelvmadrigal@gmail.com>
2017-01-11 13:18:22 -0600

    Created reusable code for Mongo calls

--- webapp.js
+++ webapp.js
@@ -5,50 +5,49 @@ import MongoClient from 'mongodb';
 var app = express();
 var db;
 
+function getFromCollection(collection, parameters) {
+  return db.collection(collection).find().toArray();
+};
+
+function addToCollection(collection, parameters) {
+  return db.collection(collection).insertOne(parameters.body).then((result) => {
+    return db.collection(collection).find({
+      _id: result.insertedId
+    }).toArray();
+  });
+}
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 });
 
 app.get('/api/jobs', (req, res) => {
-	db.collection('jobs').find().toArray((err, docs) => {
-		res.json(docs);
-	});
+  getFromCollection("jobs", "").then((docs) => {
+    res.json(docs);
+  }); 
 });
 
 app.use(bodyParser.json());
 app.post('/api/jobs', (req, res) => {
-	var newJob = req.body;
-	db.collection('jobs').insertOne(newJob, (err, result) => {
-		var newId = result.insertedId;
-		db.collection('jobs').find({
-			_id: newId
-		}).next((err, doc) => {
-			res.json(doc);
-		});
-	});
+  addToCollection("jobs", req).then((result) => {
+    res.json(result);
+  })
 });
 
 app.get('/api/reporters', (req, res) => {
-	db.collection('reporters').find().toArray((err, docs) => {
-		res.json(docs);
-	});
+  getFromCollection("reporters", "").then((docs) => {
+    res.json(docs);
+  }); 
 });
 
 app.use(bodyParser.json());
 app.post('/api/reporters', (req, res) => {
-	var newJob = req.body;
-	db.collection('reporters').insertOne(newJob, (err, result) => {
-		var newId = result.insertedId;
-		db.collection('reporters').find({
-			_id: newId
-		}).next((err, doc) => {
-			res.json(doc);
-		});
-	});
+  addToCollection("reporters", req).then((result) => {
+    res.json(result);
+  })
 });
 
-
 MongoClient.connect('mongodb://localhost/proofreading', (err, dbConnection) => {
-	db = dbConnection;
-	app.use(express.static('static'));
+  db = dbConnection;
+  app.use(express.static('static'));
 })
